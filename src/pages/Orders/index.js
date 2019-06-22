99983c56fabfc9b77d54c6b64764618b27866cce
moiseshilario <moiseshilariu@gmail.com>
2019-06-22 01:51:54 -0300

    Add auth header to api requests

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import api from '../../services/api';
 
 // import { Container } from './styles';
 
-const Orders = () => (
-  <div>
-    orders
-    <Link to="/login">Login</Link>
-  </div>
-);
+const Orders = () => {
+  useEffect(() => {
+    api.get('/test');
+  }, []);
+
+  return (
+    <div>
+      orders
+      <Link to="/login">Login</Link>
+    </div>
+  );
+};
 
 export default Orders;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
 import axios from 'axios';
+import store from '../store';
 
 const api = axios.create({
   baseURL: 'http://localhost:3333',
+});
+
+api.interceptors.request.use((config) => {
+  const { token } = store.getState().auth;
+
+  const headers = { ...config.headers };
+
+  if (token) {
+    headers.Authorization = `Bearer ${token}`;
+  }
+
+  return { ...config, headers };
 });
 
 export default api;
